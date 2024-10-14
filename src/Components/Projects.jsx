7c599da646a3026517bc1c1544b9ef91e5eca6d3
Akilraj-1153 <akilrajn1153@gmail.com>
2024-10-14 10:57:29 +0530

    changed project animation

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,27 +1,48 @@
-import React, { useContext } from "react";
+import React from "react";
+import { useContext } from "react";
 import { PortfolioContext } from "../App";
 import { projects_data } from "./Data";
 import { ExternalLink } from "react-external-link";
+import { motion, useAnimate, useInView, stagger } from "framer-motion";
+import { useEffect } from "react";
 
 const Projects = () => {
   const { NavBarHeight, isDarkTheme } = useContext(PortfolioContext);
 
+  const [scope, animate] = useAnimate();
+  const isInView = useInView(scope, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      animate([[scope.current, { opacity: 1 }]]);
+    }
+  }, [isInView]);
+
   return (
-    <div
-      className="flex flex-1"
+    <motion.div
+      ref={scope}
+      initial={{ opacity: 0 }}
+      className={` flex flex-1`}
       style={{ minHeight: `calc(100vh - ${NavBarHeight}px)` }}
     >
-      <div className="w-full flex-1">
+      <div className=" w-full  flex-1">
         <h2 className="text-center text-3xl md:text-4xl font-bold mb-8 font-mateSc">
           Projects
         </h2>
-        <div className="flex flex-col items-center p-2 gap-5 pb-10">
+        <div className={`flex flex-col items-center p-2 gap-2`}>
           {projects_data.map((projects, index) => (
-            <div
+            <motion.div
               key={index}
-              className={`max-w-4xl shadow-2xl rounded-lg flex flex-col items-center transition-colors duration-500 ${
-                isDarkTheme ? "bg-[#1E1E1E] text-white" : "bg-white text-black"
-              }`}
+              initial={{ opacity: 0, scale: 0 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              viewport={{ once: true, amount: 0.2 }}
+              className={`max-w-4xl shadow-2xl rounded-lg flex flex-col items-center
+                ${
+                  isDarkTheme
+                    ? "bg-[#1E1E1E] text-white"
+                    : "bg-white text-black"
+                }
+            `}
             >
               <div className="relative">
                 <img
@@ -29,42 +50,58 @@ const Projects = () => {
                   alt=""
                   className="h-full w-full rounded-t-lg"
                 />
-                <div className="absolute backdrop-blur-lg bg-[#1E1E1E]/50 text-white  flex flex-col items-center gap-2 bottom-0 p-3 w-full font-bold text-center transition-colors duration-500">
+                <div
+                  className={`absolute backdrop-blur-lg bg-[#1E1E1E]/50 text-white rounded-b-lg flex flex-col items-center gap-2 bottom-0 p-3 w-full font-bold text-center transition-colors duration-500`}
+                >
                   <h1 className="text-xl md:text-3xl uppercase">
                     {projects.Title}
                   </h1>
                   <div>
                     {projects.Link && (
                       <ExternalLink href={projects.Link}>
-                        <button className="capitalize cursor-pointer h-fit w-fit whitespace-nowrap text-center px-6 py-2 rounded-lg text-lg font-semibold transition-colors duration-500 bg-white text-black">
+                        <motion.button
+                          whileHover={{ scale: 1.1 }}
+                          transition={{
+                            type: "spring",
+                            stiffness: 300,
+                            damping: 10,
+                          }}
+                          className={`capitalize cursor-pointer h-fit w-fit whitespace-nowrap text-center px-6 py-2 rounded-lg text-lg font-semibold  ${
+                            isDarkTheme
+                              ? "bg-white text-black "
+                              : "bg-white text-black"
+                          } transition-colors duration-500`}
+                        >
                           {index === 2 ? "Download App" : "Visit Page"}
-                        </button>
+                        </motion.button>
                       </ExternalLink>
                     )}
 
                     {projects.version && (
-                      <div className="flex gap-2">
+                      <motion.div className="flex gap-2">
                         {projects.version.map((version, i) => (
                           <ExternalLink key={i} href={version.Link}>
-                            <button className="capitalize cursor-pointer text-center h-10 w-10 rounded-full text-lg font-semibold bg-white text-black hover:text-white hover:bg-[#1E1E1E] transition-colors duration-500">
+                            <button
+                              className={`capitalize cursor-pointer text-center h-10 w-10  rounded-full text-lg font-semibold bg-white text-black hover:text-white hover:bg-[#1E1E1E] transition-colors duration-500`}
+                            >
                               {version.versionNo}
                             </button>
                           </ExternalLink>
                         ))}
-                      </div>
+                      </motion.div>
                     )}
                   </div>
                 </div>
               </div>
 
-              <p className="text-md lg:text-lg text-justify p-4">
+              <p className="text-md lg:text-lg text-justify  p-4">
                 {projects.Description}
               </p>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
